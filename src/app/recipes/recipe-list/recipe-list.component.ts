852423dbdfb65a1fd25797d873a2cb401c52c2ca
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-06-19 08:45:58 +0200

    53-54 Outputting a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes : Recipe[] = [
+    new Recipe('A test recipe', 'This is a simple test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+    ,new Recipe('A test recipe', 'This is a simple test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+    ,new Recipe('A test recipe', 'This is a simple test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+  ];
 
 }
