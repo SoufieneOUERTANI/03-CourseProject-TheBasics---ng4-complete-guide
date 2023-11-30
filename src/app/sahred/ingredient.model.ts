3609a383c1676cbcd2eceaab161cce8da101ffaa
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-11-30 08:21:43 +0100

    60. Creating an "Ingredient" Model

---
+++ src/app/sahred/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class ingredient{
+    // automatic arguments constructor
+    // The public modifier in the constructor variables will automatically behind the scene :
+    // create the variable in the class and make the assignement 
+    constructor(public name : string, public amount : number){}
+}
